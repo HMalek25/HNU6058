4c55e4eba2e1df0ef46a5d4b24cc741fcf9f1171
Halima Malek <halimamalek@MacBookAir.ht.home>
2025-08-30 21:19:04 -0400

    Mise à jour du site avec les dernières modifications

--- src/_data/zoteroBib.js
+++ src/_data/zoteroBib.js
@@ -3,7 +3,7 @@ const EleventyFetch = require("@11ty/eleventy-fetch");
 
 async function getTopStories() {
     const apiKey = process.env.ZOTERO_API_KEY;
-    const groupID = '6129177';
+    const groupID = '322999';
     const url = `https://api.zotero.org/groups/${groupID}/items?key=${apiKey}`;
     const response = EleventyFetch(url, {
         duration: "id",
@@ -13,4 +13,4 @@ async function getTopStories() {
     return articles;
 }
 
-module.exports = getTopStories
+module.exports = getTopStories;
